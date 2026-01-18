9b8799b6f802189120be5d1a8df91ffad1457569
Nihchalk <nkaur3_be24@thapar.edu>
2026-01-18 23:49:01 +0530

    Create q3.cpp

---
+++ Lab1/q3.cpp
@@ -0,0 +1,36 @@
+//q3. Implement Quick Sort for arr[n] = { 4, 2, 6, 9, 2 }   
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int partition(vector<int>& arr, int low, int high) {
+    int pivot = arr[high];
+    int i = low - 1;
+    for (int j = low; j <= high - 1; j++) {
+        if (arr[j] < pivot) {
+            i++;
+            swap(arr[i], arr[j]);
+        }
+    }
+    swap(arr[i + 1], arr[high]);  
+    return i + 1;
+}
+void quickSort(vector<int>& arr, int low, int high) {
+  
+    if (low < high) {
+        int pIndex = partition(arr, low, high);
+        quickSort(arr, low, pIndex - 1);
+        quickSort(arr, pIndex + 1, high);
+    }
+}
+
+int main() {
+    vector<int> arr = { 4, 2, 6, 9, 2 } ;
+    int n = arr.size();
+    quickSort(arr, 0, n - 1);
+  
+    for (int i = 0; i < n; i++) {
+        cout << arr[i] << " ";
+    }
+    return 0;
+}
